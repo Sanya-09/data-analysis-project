00c8aa0a85affe3a6bb6858e3649c87876dc285c
Sanya Agarwal <agarwalsanya90@gmail.com>
2026-02-19 19:24:44 +0530

    Update Black Friday - Walkthrough.py

--- Black Friday Sales Analysis/Black Friday - Walkthrough.py
+++ Black Friday Sales Analysis/Black Friday - Walkthrough.py
@@ -1,1 +1,10 @@
 import pandas as pd
+df = pd.read_csv('BlackFriday.csv')
+print(df.head())
+print(df.info())
+print(df)
+print(df.isnull().sum())
+print(df.dropna())
+del df['Product_Category_2']
+del df['Product_Category_3']
+print(df)
